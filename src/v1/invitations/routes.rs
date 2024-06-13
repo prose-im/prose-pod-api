1a0959a410e11aef14694ba1ec849cc5e9127895
Rémi Bardon <remi@remibardon.name>
2024-06-13 17:00:27 +0200

    feat: Add a way to use JID as password when auto-accepting invitations

--- src/v1/invitations/routes.rs
+++ src/v1/invitations/routes.rs
@@ -124,16 +124,24 @@ pub(super) async fn invite_member<'r>(
     #[cfg(debug_assertions)]
     if config.debug_only.automatically_accept_invitations {
         warn!(
-            "Config `{}` is turned on. The invitation created will be automatically accepted.",
+            "Config `{}` is turned on. The created invitation will be automatically accepted.",
             stringify!(debug_only.automatically_accept_invitations),
         );
 
-        // NOTE: Code taken from <https://rust-lang-nursery.github.io/rust-cookbook/algorithms/randomness.html#create-random-passwords-from-a-set-of-alphanumeric-characters>.
-        let password = thread_rng()
-            .sample_iter(&Alphanumeric)
-            .take(32)
-            .map(char::from)
-            .collect();
+        let password = if config
+            .debug_only
+            .insecure_password_on_auto_accept_invitation
+        {
+            // Use JID as password to make password predictable
+            invitation.jid.to_string()
+        } else {
+            // NOTE: Code taken from <https://rust-lang-nursery.github.io/rust-cookbook/algorithms/randomness.html#create-random-passwords-from-a-set-of-alphanumeric-characters>.
+            thread_rng()
+                .sample_iter(&Alphanumeric)
+                .take(32)
+                .map(char::from)
+                .collect()
+        };
         invitation_accept_(
             db,
             invitation.accept_token.into(),
