58ad13f7f1e28e335d0081ae16a25af39bc4d678
Rémi Bardon <remi@remibardon.name>
2024-06-18 16:48:55 +0200

    feat(prosody-config): Externalize `linked_hash_{map,set}`

--- prosody-config/src/lib.rs
+++ prosody-config/src/lib.rs
@@ -3,6 +3,9 @@
 // Copyright: 2024, Rémi Bardon <remi@remibardon.name>
 // License: Mozilla Public License v2.0 (MPL v2.0)
 
+pub extern crate linked_hash_map;
+pub extern crate linked_hash_set;
+
 mod model;
 mod prosody_config;
 mod prosody_config_file;
