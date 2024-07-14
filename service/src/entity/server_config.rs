0c35ed6af102f6fc6eb1a948154003125cb52858
Rémi Bardon <remi@remibardon.name>
2024-07-14 18:44:31 +0200

    typo: Fix typo "overriden" -> "overridden"

--- service/src/entity/server_config.rs
+++ service/src/entity/server_config.rs
@@ -14,10 +14,10 @@ use crate::{
 ///
 /// All fields are optional because the Prose Pod API only stores manual overrides.
 /// This way, if security defaults are raised, every Prose Pod will automatically benefit from it upon update.
-/// Those default values (from [config::defaults][crate::config::defaults]) can also be overriden
+/// Those default values (from [config::defaults][crate::config::defaults]) can also be overridden
 /// by a Prose Pod administrator via the Prose Pod API configuration file (`Prose.toml`).
 ///
-/// When returning the server configuration, the Prose Pod API replaces non-overriden (empty) values
+/// When returning the server configuration, the Prose Pod API replaces non-overridden (empty) values
 /// by their default. See [ServerConfig] and [Model::with_default_values].
 #[derive(Clone, Debug, PartialEq, DeriveEntityModel, Eq)]
 #[sea_orm(table_name = "server_config")]
