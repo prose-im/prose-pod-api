6cdb040f44cce146273a462a7add33ab700bf488
Rémi Bardon <remi@remibardon.name>
2024-08-26 16:34:11 +0200

    fix: Fix Prose Pod API Docker image not prefixed correctly

--- scripts/constants.sh
+++ scripts/constants.sh
@@ -1,13 +1,19 @@
-image_name() {
+# ===== Define local heplers =====
+
+image_name_() {
 	echo "${PROSE_DOCKER_ORG:?}/${1:?}:${DOCKER_TAG:?}"
 }
-archive_name() {
+archive_name_() {
 	echo $@ | sed -e 's#/#%2F#' -e 's/:/%3A/'
 }
 
-PROSE_DOCKER_ORG=proseim
-DOCKER_TAG=latest
-PROSE_POD_SERVER_IMAGE=$(image_name prose-pod-server)
-PROSE_POD_SERVER_ARCHIVE=$(archive_name $PROSE_POD_SERVER_IMAGE)
-PROSE_POD_API_IMAGE=$(image_name prose-pod-api)
-PROSE_POD_API_ARCHIVE=$(archive_name $PROSE_POD_API_IMAGE)
+# ===== Define constants =====
+# NOTE: The `: ${VAR_NAME:=value}` syntax initializes a variable only if it's unset or null.
+#   It avoids resetting a variable when sourcing this file after the variable was overriden.
+
+: ${PROSE_DOCKER_ORG:=proseim}
+: ${DOCKER_TAG:=latest}
+: ${PROSE_POD_SERVER_IMAGE:=$(image_name_ prose-pod-server)}
+: ${PROSE_POD_SERVER_ARCHIVE:=$(archive_name_ $PROSE_POD_SERVER_IMAGE)}
+: ${PROSE_POD_API_IMAGE:=$(image_name_ prose-pod-api)}
+: ${PROSE_POD_API_ARCHIVE:=$(archive_name_ $PROSE_POD_API_IMAGE)}
--- scripts/prose-pod-on-rpi/constants.sh
+++ scripts/prose-pod-on-rpi/constants.sh
@@ -1,21 +1,31 @@
+# ===== Source higher-level constants =====
+
 source "${SCRIPTS_ROOT:?}"/constants.sh
 
+# ===== Define constants =====
+# NOTE: The `: ${VAR_NAME:=value}` syntax initializes a variable only if it's unset or null
+#   and the `: ${VAR_NAME=value}` syntax initializes a variable only if it's unset.
+#   It avoids resetting a variable when sourcing this file after the variable was overriden.
+
 # Temporary directory used by this script.
-PROSE_TMPDIR="${TMPDIR%/}"/org.prose.pod.test
+: ${PROSE_TMPDIR:="${TMPDIR%/}"/org.prose.pod.test}
 
 # TODO: Allow choosing `TARGET_ARCH` and `DOCKER_ARCH_PREFIX` via command line arguments
 #   to better support other Raspberry Pi versions.
 
 # `cargo` target to use when building.
 # TIP: See all possible values using `cargo target list`.
-TARGET_ARCH="${TARGET_ARCH-"armv7-unknown-linux-musleabihf"}"
+: ${TARGET_ARCH="armv7-unknown-linux-musleabihf"}
 # Prefix image names so it doesn't conflict with the ones used locally.
 # NOTE: Value comes from <https://github.com/docker-library/official-images?tab=readme-ov-file#architectures-other-than-amd64>.
-DOCKER_ARCH_PREFIX="${DOCKER_ARCH_PREFIX-"arm32v7"}"
+: ${DOCKER_ARCH_PREFIX="arm32v7"}
+
 # NOTE: `PROSE_POD_*_ARCHIVE` variables have already been generated so
 #   the image names won't be prefixed on the RPi (that's what we want).
-PROSE_POD_API_IMAGE="${DOCKER_ARCH_PREFIX}/${PROSE_POD_API_IMAGE:?}"
-PROSE_POD_SERVER_IMAGE="${DOCKER_ARCH_PREFIX}/${PROSE_POD_SERVER_IMAGE:?}"
+: ${PREFIXED_PROSE_POD_API_IMAGE:="${DOCKER_ARCH_PREFIX}/${PROSE_POD_API_IMAGE:?}"}
+PROSE_POD_API_IMAGE="${PREFIXED_PROSE_POD_API_IMAGE}"
+: ${PREFIXED_PROSE_POD_SERVER_IMAGE:="${DOCKER_ARCH_PREFIX}/${PROSE_POD_SERVER_IMAGE:?}"}
+PROSE_POD_SERVER_IMAGE="${PREFIXED_PROSE_POD_SERVER_IMAGE}"
 
-REMOTE_PROSE_POD_SYSTEM_DIR=${REMOTE_PROSE_POD_SYSTEM_DIR-"prose-pod-system"};
-REMOTE_SERVER_ROOT=${REMOTE_SERVER_ROOT-"${REMOTE_PROSE_POD_SYSTEM_DIR:?}"/server/pod};
+: ${REMOTE_PROSE_POD_SYSTEM_DIR="prose-pod-system"}
+: ${REMOTE_SERVER_ROOT="${REMOTE_PROSE_POD_SYSTEM_DIR:?}"/server/pod}
