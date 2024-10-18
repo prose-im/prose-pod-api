008d376fa767498edd5e34aa72008138a7c6ea3a
Rémi Bardon <remi@remibardon.name>
2024-10-18 15:14:20 +0200

    wip: test(dns): Implement DNS-related integration tests
    Also fix asynchrony and serialization issues.

--- crates/api/src/v1/network/checks/routes.rs
+++ crates/api/src/v1/network/checks/routes.rs
@@ -280,7 +280,7 @@ pub(super) async fn check_ip_stream<'r>(
 pub struct NetworkCheckResult {
     id: String,
     event: NetworkCheckEvent,
-    data: String,
+    data: serde_json::Value,
 }
 
 impl NetworkCheckResult {
@@ -298,7 +298,7 @@ impl NetworkCheckResult {
         Self {
             event: NetworkCheckEvent::from(&check),
             id: Id::from(check.to_owned()).to_string(),
-            data: serde_json::to_string(&data).unwrap_or_default(),
+            data: serde_json::to_value(&data).unwrap_or_default(),
         }
     }
 }
