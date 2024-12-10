c969d16b6c23fa5b48a3a38338c83817bd776b15
Rémi Bardon <remi@remibardon.name>
2024-12-10 16:19:04 +0100

    chore: Run locally using tagged images instead of forcing local builds

--- scripts/constants.sh
+++ scripts/constants.sh
@@ -1,10 +1,13 @@
 # ===== Define local heplers =====
 
 image_name_() {
-	echo "${PROSE_DOCKER_ORG:?}/${1:?}:${DOCKER_TAG:?}"
+	local name="${1:?}"
+	echo "${PROSE_DOCKER_ORG:?}/${name:?}"
 }
-archive_name_() {
-	echo $@ | sed -e 's#/#%2F#' -e 's/:/%3A/'
+image_name_full_() {
+	local name="${1:?}"
+	local tag="${2:-"${DEFAULT_DOCKER_TAG:?}"}"
+	echo "${name:?}:${tag:?}"
 }
 
 # ===== Define constants =====
@@ -12,8 +15,14 @@ archive_name_() {
 #   It avoids resetting a variable when sourcing this file after the variable was overriden.
 
 : ${PROSE_DOCKER_ORG:=proseim}
-: ${DOCKER_TAG:=latest}
-: ${PROSE_POD_SERVER_IMAGE:=$(image_name_ prose-pod-server)}
-: ${PROSE_POD_SERVER_ARCHIVE:=$(archive_name_ $PROSE_POD_SERVER_IMAGE)}
-: ${PROSE_POD_API_IMAGE:=$(image_name_ prose-pod-api)}
-: ${PROSE_POD_API_ARCHIVE:=$(archive_name_ $PROSE_POD_API_IMAGE)}
+: ${DEFAULT_DOCKER_TAG:=latest}
+# Tag used to reference images built locally (e.g. uncommitted code).
+: ${LOCAL_DOCKER_TAG:=local}
+
+: ${PROSE_POD_SERVER_IMAGE_NAME:=$(image_name_ prose-pod-server)}
+: ${PROSE_POD_SERVER_IMAGE_TAG:=${DEFAULT_DOCKER_TAG:?}}
+: ${PROSE_POD_SERVER_IMAGE:=$(image_name_full_ "${PROSE_POD_SERVER_IMAGE_NAME:?}" "${PROSE_POD_SERVER_IMAGE_TAG}")}
+
+: ${PROSE_POD_API_IMAGE_NAME:=$(image_name_ prose-pod-api)}
+: ${PROSE_POD_API_IMAGE_TAG:=${DEFAULT_DOCKER_TAG:?}}
+: ${PROSE_POD_API_IMAGE:=$(image_name_full_ "${PROSE_POD_API_IMAGE_NAME:?}" "${PROSE_POD_API_IMAGE_TAG}")}
