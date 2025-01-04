71919ff09668124497f6fe2504b569570ed6bf30
Rémi Bardon <remi@remibardon.name>
2025-01-04 02:37:18 +0100

    fix(startup): Run startup actions before starting the API

--- crates/rest-api/src/lib.rs
+++ crates/rest-api/src/lib.rs
@@ -12,6 +12,7 @@ pub mod responders;
 pub mod util;
 
 use axum::{http::StatusCode, routing::get_service, Router};
+use features::startup_actions::on_startup;
 use service::{
     auth::AuthService,
     dependencies::Uuid,
@@ -67,11 +68,16 @@ impl AppState {
 
 impl AxumState for AppState {}
 
+#[derive(Debug, thiserror::Error)]
+#[error("Startup error: {0}")]
+pub struct StartupError(String);
+
 /// A custom [`Router`] with a default configuration.
-pub fn custom_router(app_state: AppState) -> Router {
-    // on_startup(&app_state).await?;
-    Router::new()
-        .merge(features::router(app_state))
+pub async fn custom_router(app_state: AppState) -> Result<Router, StartupError> {
+    // Create the router first, because it's a cheap operation and
+    // Axum validates that there are no overlapping routes.
+    let router = Router::new()
+        .merge(features::router(app_state.clone()))
         .nest_service(
             "/api-docs",
             get_service(ServeDir::new("static/api-docs")).handle_error(|error| async move {
@@ -80,6 +86,19 @@ pub fn custom_router(app_state: AppState) -> Router {
                     format!("Unhandled internal error: {error}"),
                 )
             }),
-        )
+        );
     // .register("/", catchers![default_catcher])
+
+    // Before returning the router, run startup actions to ensure
+    // everything works correctly when the API launches.
+    //
+    // If we did that in `main`, we'd have to add it in both the tests' `main`
+    // function and in the API's `main` function.
+    //
+    // Instead of relying on integration tests to detect that we forgot to
+    // run startup actions in the API's `main` function, we can do it
+    // in this shared code.
+    on_startup(&app_state).await.map_err(StartupError)?;
+
+    Ok(router)
 }
