6b83fbb3651ecef53c602a0bc6f2cc8b1d32c452
Rémi Bardon <remi@remibardon.name>
2025-01-11 17:53:30 +0100

    feat: Allow one to run an ephemeral scenario

--- local-run/scripts/constants.sh
+++ local-run/scripts/constants.sh
@@ -7,6 +7,7 @@ LOCAL_RUN_DIR="${PROSE_POD_API_DIR:?}"/local-run
 : ${COMPOSE_FILE:="${LOCAL_RUN_DIR:?}"/compose.yaml}
 DEFAULT_SCENARIO_NAME=default
 SCENARIOS_DIR="${LOCAL_RUN_DIR:?}"/scenarios
+EPHEMERAL_SCENARIO_NAME_FILE="${SCENARIOS_DIR:?}"/last-ephemeral-scenario-name.txt
 
 source "${SCRIPTS_ROOT:?}"/constants.sh
 source "${SCRIPTS_ROOT:?}"/image-names.sh
