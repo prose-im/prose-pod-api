7860f52494f8886e3e29f9fa58bd54aff5bf110f
Rémi Bardon <remi@remibardon.name>
2025-01-31 03:15:04 +0100

    v0.8.2

--- local-run/scripts/constants.sh
+++ local-run/scripts/constants.sh
@@ -1,7 +1,7 @@
 # NOTE: The `: ${VAR_NAME:=value}` syntax initializes a variable only if it's unset or null.
 #   It avoids resetting a variable when sourcing this file after the variable was overriden.
 
-: ${PROSE_POD_API_IMAGE_TAG:=0.8.1}
+: ${PROSE_POD_API_IMAGE_TAG:=0.8.2}
 : ${PROSE_POD_SERVER_IMAGE_TAG:=0.3.7}
 LOCAL_RUN_DIR="${PROSE_POD_API_DIR:?}"/local-run
 : ${COMPOSE_FILE:="${LOCAL_RUN_DIR:?}"/compose.yaml}
