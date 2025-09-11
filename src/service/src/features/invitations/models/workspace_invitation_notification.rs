196f47e2cc40cc0049c4c4256ef3ab14513c412a
Rémi Bardon <remi@remibardon.name>
2025-09-11 11:29:35 +0700

    fix(invitations): `"You have been invited to {company}'s Prose server!"` isn't correct with company names ending with a “s”
    Fixes #119.

--- src/service/src/features/invitations/models/workspace_invitation_notification.rs
+++ src/service/src/features/invitations/models/workspace_invitation_notification.rs
@@ -49,7 +49,11 @@ fn notification_subject(
     }: &WorkspaceInvitationPayload,
 ) -> String {
     if let Some(ref company) = organization_name {
-        format!("You have been invited to {company}’s Prose server!")
+        if company.ends_with("s") {
+            format!("You have been invited to {company}’ Prose server!")
+        } else {
+            format!("You have been invited to {company}’s Prose server!")
+        }
     } else {
         format!("You have been invited to {workspace_name}!")
     }
@@ -67,7 +71,11 @@ fn notification_message(
 ) -> String {
     vec![
         if let Some(ref company) = organization_name {
-            format!("You have been invited to {company}’s Prose server!")
+            if company.ends_with("s") {
+                format!("You have been invited to {company}’ Prose server!")
+            } else {
+                format!("You have been invited to {company}’s Prose server!")
+            }
         } else {
             format!("You have been invited to {workspace_name}!")
         }
